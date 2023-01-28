cd4d2b6e263d472a512eafb0ddce086106e5f8a5
Lael Siqueira Martinez Junior <laelsiqueiramartinezjunior@gmail.com>
2023-01-28 13:56:08 -0300

    Trabalho Modulo 2 TDD

--- src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,26 +1,27 @@
 package com.devsuperior.bds02.controllers;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.bds02.dto.CityDTO;
-import com.devsuperior.bds02.services.CityService;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventService;
 
 @RestController
 @RequestMapping(value = "/events")
 public class EventController {
 
 	@Autowired
-	private CityService service;
+	private EventService service;
 	
-	@GetMapping
-	public ResponseEntity<List<CityDTO>> findAll(Pageable pageable) {
-		List<CityDTO> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
+
 }
